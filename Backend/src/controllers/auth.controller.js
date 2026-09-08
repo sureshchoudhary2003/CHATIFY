c5957f2c9579c176882b85fc05d330009d95e7df
suresh choudhary <choudharysuresh0958@gmail.com>
2026-09-08 19:25:08 +0530

    login-logout implementation

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -95,7 +95,8 @@ export const login = async (req, res) => {
       profilePic: user.profilePic,
     });
   } catch (error) {
-    console.error("Error in login controller:", error);
+    
+    console.error("Error in login controller :", error);
     res.status(500).json({ message: "Internal server error" });
   }
 };
--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -7,6 +7,6 @@ router.post('/signup',signup);
 
 router.post('/Login',login);
 
-router.post('/logout',logout);
+router.post('/logout', logout);
 
 export default router;
