caf67dc2738dd8c5d5604962b6a7b7824849dd70
suresh choudhary <choudharysuresh0958@gmail.com>
2026-09-08 22:03:44 +0530

    rate-limiting(arcjet) implemented

--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -1,13 +1,19 @@
 import express from "express";
 import { login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
-
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 const router = express.Router();
 
+//test rate limit
+// router.get("/test", arcjetProtection, (req, res) => res.status(200).json({
+//     message: "Test route",
+// }));
+
+router.use(arcjetProtection);//check middleware in before all following req 
 
 router.post('/signup',signup);
 
-router.post('/Login',login);
+router.post('/Login',arcjetProtection,login);
 
 router.post('/logout', logout);
 
